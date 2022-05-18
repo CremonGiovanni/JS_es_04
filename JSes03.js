78b66a749e0b17096aabc26ab3174f73d756d9c9
cremon.giovanni <cremon.giovanni@studenti.salesianisesto.it>
2022-05-18 09:31:45 +0200

    pop up

--- JSes03.js
+++ JSes03.js
@@ -1,50 +1,58 @@
 var x = 3;
 var sing;
-$("#btn1").click(function (){calcola("+")});
-$("#btn2").click(function (){calcola("-")});
-$("#btn3").click(function (){calcola("*")});
-$("#btn4").click(function (){calcola("/")});
+$("#btn1").click(function () {
+  calcola("+");
+});
+$("#btn2").click(function () {
+  calcola("-");
+});
+$("#btn3").click(function () {
+  calcola("*");
+});
+$("#btn4").click(function () {
+  calcola("/");
+});
 
 function calcola(sing) {
-  var n1 = $("#N1").val();
-  var n2 = $("#N2").val();
-  if(!n1==null&&!n2==null){
+  if ($("#N1").val() && $("#N2").val()) {
+    var n1 = $("#N1").val();
+    var n2 = $("#N2").val();
+
     switch (sing) {
-        case "+":
-          var res = n1 - -n2;
-    
-          break;
-        case "-":
-          var res = n1 - n2;
-    
-          break;
-        case "*":
-          var res = n1 * n2;
-    
-          break;
-        case "/":
-          var res = n1 / n2;
-          break;
-      }
-      console.log(res);
-      $("#tb").append(
-        "<tr><td>" +
-          x +
-          "</td><td>" +
-          n1 +
-          "</td><td>" +
-          sing +
-          "</td><td>" +
-          n2 +
-          "</td><td>" +
-          res +
-          "</td></tr>"
-      );
-      console.log($("#tb"));
-      x++;
-      console.log("x=" + x);
-  }else{
-    window.alert("complete all input");
+      case "+":
+        var res = n1 - -n2;
+
+        break;
+      case "-":
+        var res = n1 - n2;
+
+        break;
+      case "*":
+        var res = n1 * n2;
+
+        break;
+      case "/":
+        var res = n1 / n2;
+        break;
+    }
+    console.log(res);
+    $("#tb").append(
+      "<tr><td>" +
+        x +
+        "</td><td>" +
+        n1 +
+        "</td><td>" +
+        sing +
+        "</td><td>" +
+        n2 +
+        "</td><td>" +
+        res +
+        "</td></tr>"
+    );
+    console.log($("#tb"));
+    x++;
+    console.log("x=" + x);
+  } else {
+    alert("complete all input");
   }
-  
-};
+}
