4a958bcd6ffb5d7e370e493dc2b545e15fc721f5
cremon.giovanni <cremon.giovanni@studenti.salesianisesto.it>
2022-05-18 09:16:40 +0200

    optimization and pop up

--- JSes03.js
+++ JSes03.js
@@ -1,37 +1,50 @@
-var x=3;
-$("#btn1").click(function () {
+var x = 3;
+var sing;
+$("#btn1").click(function (){calcola("+")});
+$("#btn2").click(function (){calcola("-")});
+$("#btn3").click(function (){calcola("*")});
+$("#btn4").click(function (){calcola("/")});
+
+function calcola(sing) {
   var n1 = $("#N1").val();
   var n2 = $("#N2").val();
-  console.log(n1 -(-n2));
-  $("#tb").append("<tr><td>"+x+"</td><td>"+n1+"</td><td>+</td><td>"+ n2+"</td><td>"+(n1 -(-n2))+"</td></tr>");
-  console.log($("#tb"));
-  x++;
-  console.log("x="+x);
-});
-$("#btn2").click(function () {
-  var n1 = $("#N1").val();
-  var n2 = $("#N2").val();
-  console.log(n1 -n2);
-  $("#tb").append("<tr><td>"+x+"</td><td>"+n1+"</td><td>-</td><td>"+ n2+"</td><td>"+(n1-n2)+"</td></tr>");
-  console.log($("#tb"));
-  x++;
-  console.log("x="+x);
-});
-$("#btn3").click(function () {
-  var n1 = $("#N1").val();
-  var n2 = $("#N2").val();
-  console.log(n1 *n2);
-  $("#tb").append("<tr><td>"+x+"</td><td>"+n1+"</td><td>*</td><td>"+ n2+"</td><td>"+(n1*n2)+"</td></tr>");
-  console.log($("#tb"));
-  x++;
-  console.log("x="+x);
-});
-$("#btn4").click(function () {
-  var n1 = $("#N1").val();
-  var n2 = $("#N2").val();
-  console.log(n1 /n2);
-  console.log($("#tb"));
-  $("#tb").append("<tr><td>"+x+"</td><td>"+n1+"</td><td>/</td><td>"+ n2+"</td><td>"+(n1/n2)+"</td></tr>");
-  x++;
-  console.log("x="+x);
-});
+  if(!n1==null&&!n2==null){
+    switch (sing) {
+        case "+":
+          var res = n1 - -n2;
+    
+          break;
+        case "-":
+          var res = n1 - n2;
+    
+          break;
+        case "*":
+          var res = n1 * n2;
+    
+          break;
+        case "/":
+          var res = n1 / n2;
+          break;
+      }
+      console.log(res);
+      $("#tb").append(
+        "<tr><td>" +
+          x +
+          "</td><td>" +
+          n1 +
+          "</td><td>" +
+          sing +
+          "</td><td>" +
+          n2 +
+          "</td><td>" +
+          res +
+          "</td></tr>"
+      );
+      console.log($("#tb"));
+      x++;
+      console.log("x=" + x);
+  }else{
+    window.alert("complete all input");
+  }
+  
+};
